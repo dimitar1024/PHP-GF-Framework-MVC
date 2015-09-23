99c3994401e71d053d5f987a69d9d94c4e1edde4
Dimitar Georgiev <dimitar1024@abv.bg>
2015-09-23 23:00:24 +0300

    Create Config.php file. Adding default instance on Config in App.php. Fix problem with $this in App

--- App.php
+++ App.php
@@ -1,29 +1,47 @@
 <?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of App
+ *
+ * @author dimitar1024
+ */
+
 namespace GF;
 
-include_once  'Loader.php';
+include_once 'Loader.php';
+
 class App {
+
     private static $_instance = null;
+    private $_config = null;
     
     private function __construct() {
         \GF\Loader::registerNamespace('GF', dirname(__FILE__) . DIRECTORY_SEPARATOR);
         \GF\Loader::registerAutoLoad();
     }
 
-    public function Run() {
-        
+    public function run() {
+         $this->_config = \GF\Config::getInstance();
+       //if config folder is not set, use defaultone
+        if ($this->_config->getConfigFolder() == null) {
+            $this->setConfigFolder('../config');
+        }
     }
 
     /**
      * 
      * @return \GF\App
      */
-    public static function getInstance()
-    {
+    public static function getInstance() {
         if (self::$_instance == null) {
             self::$_instance = new \GF\App();
         }
-        
         return self::$_instance;
     }
 }
+
